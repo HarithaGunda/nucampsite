2a70fe27d237fdd83c9a97b73d59ec87c720304f
Haritha Gunda <gunda.haritha@gmail.com>
2019-12-07 10:34:30 -0500

    Added the week4 nucamps workshop solutions

--- js/script.js
+++ js/script.js
@@ -16,4 +16,10 @@ $(function () {
             $("#carouselButton").children("i").addClass("fa-pause");
         }
     });
+    $("#reserveButton").click(function () {
+        $('#reserveModal').modal('show');
+    });
+    $("#loginButton").click(function () {
+        $('#loginModal').modal('show');
+    });
 })
